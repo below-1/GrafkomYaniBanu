6706a8b0d3adc663ca74ae79081b2bfe824b7908
Jordan Meta <jordan@esphere.id>
2025-04-27 14:32:31 +0800

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            metodeChoices = new ComboBox();
+            label2 = new Label();
+            y2Field = new NumericUpDown();
+            y1Field = new NumericUpDown();
+            x2Field = new NumericUpDown();
+            x1Field = new NumericUpDown();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)y2Field).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)y1Field).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)x2Field).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)x1Field).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Titik Awal";
+            label1.Click += label1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(metodeChoices);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(y2Field);
+            groupBox1.Controls.Add(y1Field);
+            groupBox1.Controls.Add(x2Field);
+            groupBox1.Controls.Add(x1Field);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 426);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Input";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 138);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Metode";
+            // 
+            // metodeChoices
+            // 
+            metodeChoices.FormattingEnabled = true;
+            metodeChoices.Items.AddRange(new object[] { "Brute Force", "DDA", "Bressenham" });
+            metodeChoices.Location = new Point(6, 156);
+            metodeChoices.Name = "metodeChoices";
+            metodeChoices.Size = new Size(188, 23);
+            metodeChoices.TabIndex = 11;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Titik Akhir";
+            label2.Click += label2_Click;
+            // 
+            // y2Field
+            // 
+            y2Field.Location = new Point(101, 100);
+            y2Field.Name = "y2Field";
+            y2Field.Size = new Size(89, 23);
+            y2Field.TabIndex = 7;
+            y2Field.ValueChanged += y2Field_ValueChanged;
+            // 
+            // y1Field
+            // 
+            y1Field.Location = new Point(101, 45);
+            y1Field.Name = "y1Field";
+            y1Field.Size = new Size(89, 23);
+            y1Field.TabIndex = 6;
+            // 
+            // x2Field
+            // 
+            x2Field.Location = new Point(6, 100);
+            x2Field.Name = "x2Field";
+            x2Field.Size = new Size(89, 23);
+            x2Field.TabIndex = 5;
+            x2Field.ValueChanged += x2Field_ValueChanged;
+            // 
+            // x1Field
+            // 
+            x1Field.Location = new Point(6, 45);
+            x1Field.Name = "x1Field";
+            x1Field.Size = new Size(89, 23);
+            x1Field.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(11, 395);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(218, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(700, 500);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(931, 546);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)y2Field).EndInit();
+            ((System.ComponentModel.ISupportInitialize)y1Field).EndInit();
+            ((System.ComponentModel.ISupportInitialize)x2Field).EndInit();
+            ((System.ComponentModel.ISupportInitialize)x1Field).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label label1;
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private NumericUpDown x1Field;
+        private NumericUpDown x2Field;
+        private NumericUpDown y1Field;
+        private NumericUpDown y2Field;
+        private Label label2;
+        private Label label5;
+        private ComboBox metodeChoices;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,347 @@
+using SkiaSharp;
+using System.Runtime.Serialization;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly int W = 700;
+        private static readonly int H = 500;
+        private static readonly int XDIV = 30;
+        private static readonly int YDIV = 28;
+        private static readonly int HLENGTH = W / XDIV;
+        private static readonly int VLENGTH = H / YDIV;
+        private static readonly int MIN_X = -1 * XDIV / 2;
+        private static readonly int MIN_Y = -1 * YDIV / 2;
+        private static readonly int MAX_X = 1 * XDIV / 2;
+        private static readonly int MAX_Y = 1 * YDIV / 2;
+
+        public int x1 { get; set; } = 0;
+        public int x2 { get; set; } = 0;
+
+        public int y1 { get; set; } = 0;
+        public int y2 { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            metodeChoices.SelectedIndex = 0;
+
+            x1Field.Minimum = MIN_X;
+            x1Field.Maximum = MAX_X;
+
+            x2Field.Minimum = MIN_X;
+            x2Field.Maximum = MAX_X;
+
+            y1Field.Minimum = MIN_Y;
+            y1Field.Maximum = MAX_Y;
+
+            y2Field.Minimum = MIN_Y;
+            y2Field.Maximum = MAX_Y;
+
+            x1Field.Value = 4;
+            y1Field.Value = 3;
+
+            x2Field.Value = 7;
+            y2Field.Value = 8;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SKImageInfo imageInfo = new SKImageInfo(W, H, SKColorType.Rgba8888, SKAlphaType.Premul);
+                using (SKSurface surface = SKSurface.Create(imageInfo))
+                {
+                    SKCanvas canvas = surface.Canvas;
+                    canvas.Clear(SKColor.Parse("ccc"));
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.Color = SKColors.Blue;
+                        paint.StrokeWidth = 15;
+                        paint.Style = SKPaintStyle.Stroke;
+
+                        DrawGrid(canvas);
+                        int x1 = Convert.ToInt32(x1Field.Value) + XDIV / 2;
+                        int x2 = Convert.ToInt32(x2Field.Value) + XDIV / 2;
+                        int y1 = (YDIV / 2) - Convert.ToInt32(y1Field.Value);
+                        int y2 = (YDIV / 2) - Convert.ToInt32(y2Field.Value);
+                        SKPoint p0 = new SKPoint(x1, y1);
+                        SKPoint p1 = new SKPoint(x2, y2);
+                        //var p0 = new SKPoint(0, 0);
+                        //var p1 = new SKPoint(12, 8);
+                        //if (p0.X > p1.X)
+                        //{
+                        //    var _p = p0;
+                        //    p0 = p1;
+                        //    p1 = _p;
+                        //}
+                        var method = metodeChoices.SelectedItem == null ? "Brute Force" : metodeChoices.SelectedItem;
+                        // var method = "DDA";
+                        DrawLine((string)method, canvas, p0, p1);
+                    }
+                    using (SKImage image = surface.Snapshot())
+                    using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
+                    using (MemoryStream mStream = new MemoryStream(data.ToArray()))
+                    {
+                        Bitmap bitmap = new Bitmap(mStream, false);
+                        pictureBox1.Image = bitmap;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+
+        protected static void DrawGrid(SKCanvas canvas)
+        {
+            var xDiv = XDIV;
+            var yDiv = YDIV;
+            var hlength = HLENGTH;
+            var vlength = VLENGTH;
+            var hcenter = W / 2;
+            var vcenter = H / 2;
+
+            using (SKPaint paint = new SKPaint())
+            using (SKPaint boldPaint = new SKPaint())
+            {
+                paint.Color = SKColor.Parse("bbb");
+                paint.StrokeWidth = 1;
+                paint.Style = SKPaintStyle.Stroke;
+
+                boldPaint.Color = SKColor.Parse("bbb");
+                boldPaint.StrokeWidth = 2;
+                boldPaint.Style = SKPaintStyle.Stroke;
+
+                //canvas.DrawLine(
+                //    new SKPoint(hcenter, 0),
+                //    new SKPoint(hcenter, H),
+                //    boldPaint
+                //);
+                //canvas.DrawLine(
+                //    new SKPoint(0, vcenter),
+                //    new SKPoint(W, vcenter),
+                //    boldPaint
+                //);
+
+                for (int i = 1; i < xDiv; i++)
+                {
+                    canvas.DrawLine(
+                        new SKPoint(i * hlength, H),
+                        new SKPoint(i * hlength, 0),
+                        paint
+                    );
+                }
+                for (int i = 1; i < yDiv; i++)
+                {
+                    var sect = i * vlength;
+                    canvas.DrawLine(
+                        new SKPoint(0, sect),
+                        new SKPoint(W, sect),
+                        paint
+                    );
+                }
+            }
+        }
+
+        protected static void DrawLine(string method, SKCanvas canvas, SKPoint p0, SKPoint p1)
+        {
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.Color = SKColors.Coral;
+                paint.StrokeWidth = 1;
+                paint.Style = SKPaintStyle.Fill;
+
+                if (method == "Brute Force")
+                {
+                    BruteForcePlotLine(canvas, paint, p0, p1);
+                }
+                else if (method == "Bressenham")
+                {
+                    BressenhamPlotLine(canvas, paint, p0, p1);
+                }
+                else if (method == "DDA")
+                {
+                    DDAPlotLine(canvas, paint, p0, p1);
+                }
+            }
+        }
+
+        protected static void BressenhamPlotLineLow(SKCanvas canvas, SKPaint paint, SKPoint p0, SKPoint p1)
+        {
+            var dx = (p1.X - p0.X);
+            var dy = (p1.Y - p0.Y);
+            var yi = 1;
+            if (dy < 0)
+            {
+                yi = -1;
+                dy = -dy;
+            }
+            var D = (2 * dy) - dx;
+            var y = p0.Y;
+            var start = (int)p0.X;
+            for (int i = start; i < p1.X; i++)
+            {
+                DrawPixel(canvas, paint, i, (int)y);
+                if (D > 0)
+                {
+                    y = y + yi;
+                    D = D + (2 * (dy - dx));
+                }
+                else
+                {
+                    D = D + (2 * dy);
+                }
+            }
+        }
+
+        protected static void BressenhamPlotLineHigh(SKCanvas canvas, SKPaint paint, SKPoint p0, SKPoint p1)
+        {
+            var dx = (p1.X - p0.X);
+            var dy = (p1.Y - p0.Y);
+            var xi = 1;
+            if (dx < 0)
+            {
+                xi = -1;
+                dx = -dx;
+            }
+            var D = (2 * dx) - dy;
+            var x = p0.X;
+            var start = (int)p0.Y;
+            for (int i = start; i < p1.Y; i++)
+            {
+                DrawPixel(canvas, paint, (int)x, i);
+                if (D > 0)
+                {
+                    x = x + xi;
+                    D = D + (2 * (dx - dy));
+                }
+                else
+                {
+                    D = D + (2 * dy);
+                }
+            }
+        }
+
+        protected static void BressenhamPlotLine(SKCanvas canvas, SKPaint paint, SKPoint p0, SKPoint p1)
+        {
+            if (Math.Abs(p1.Y - p0.Y) < Math.Abs(p1.X - p0.X))
+            {
+                if (p0.X > p1.X)
+                {
+                    BressenhamPlotLineLow(canvas, paint, p1, p0);
+                }
+                else
+                {
+                    BressenhamPlotLineLow(canvas, paint, p0, p1);
+                }
+            }
+            else
+            {
+                if (p0.Y > p1.Y)
+                {
+                    BressenhamPlotLineHigh(canvas, paint, p1, p0);
+                }
+                else
+                {
+                    BressenhamPlotLineHigh(canvas, paint, p0, p1);
+                }
+            }
+        }
+
+        protected static void DDAPlotLine(SKCanvas canvas, SKPaint paint, SKPoint p0, SKPoint p1)
+        {
+            // Calculate dx and dy 
+            var dx = p1.X - p0.X;
+            var dy = p1.Y - p0.Y;
+
+            int step;
+
+            // If dx > dy we will take step as dx 
+            // else we will take step as dy to draw the complete 
+            // line 
+            if (Math.Abs(dx) > Math.Abs(dy))
+                step = (int)Math.Abs(dx);
+            else
+                step = (int)Math.Abs(dy);
+
+            // Calculate x-increment and y-increment for each 
+            // step 
+            float x_incr = (float)dx / step;
+            float y_incr = (float)dy / step;
+
+            // Take the initial points as x and y 
+            float x = p0.X;
+            float y = p0.Y;
+
+            for (int i = 0; i < step; i++)
+            {
+
+                DrawPixel(canvas, paint, (int)x, (int)y);
+                x += x_incr;
+                y += y_incr;
+                // delay(10); 
+            }
+        }
+
+        protected static void BruteForcePlotLine(SKCanvas canvas, SKPaint sKPaint, SKPoint p0, SKPoint p1)
+        {
+            var m = (p1.Y - p0.Y) / (p1.X - p0.X);
+            var N = p1.X - p0.X + 1;
+            var x = p0.X;
+            for (int i = 0; i < N; i++)
+            {
+                var y = m * (x - p0.X) + p0.Y;
+                float ya = Round(y);
+                DrawPixel(canvas, sKPaint, (int)x, (int)y);
+                x += 1;
+            }
+        }
+
+        protected static void DrawPixel(SKCanvas canvas, SKPaint paint, int x, int y)
+        {
+            var _x = (int)(x * HLENGTH);
+            var _y = (int)(y * VLENGTH);
+            var rect = new SKRect(_x, _y, _x + HLENGTH, _y + VLENGTH);
+            canvas.DrawRect(rect, paint);
+        }
+
+        protected static (int, int) ConvertGridToRealCoordintates(int x, int y)
+        {
+            return (x * HLENGTH, y * VLENGTH);
+        }
+
+        private void y2Field_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int Round(float n)
+        {
+            if (n - (int)n < 0.5)
+                return (int)n;
+            return (int)(n + 1);
+        }
+
+        private void x2Field_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
